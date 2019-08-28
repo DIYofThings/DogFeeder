845ed4d301b631dfc2a14c49026420638f129828
aiscript <aiscript@gmail.com>
2019-08-29 05:33:31 +0700

    Dog Feeder

---
+++ BLYNK-DogFeeder.ino
@@ -0,0 +1,82 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Servo.h>
+
+Servo petGate;
+int gateStart = 45;
+int gateEnd = 105;
+int gateOpenTime = 150;
+
+char auth[] = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "xxxxxx";
+char pass[] = "xxxxxx";
+
+BLYNK_WRITE(V0)
+{
+  petGate.write(gateEnd);
+  delay(gateOpenTime);
+  petGate.write(gateStart);
+}
+
+BLYNK_WRITE(V1)
+{
+  for(int i=0;i<5;i++)
+  {
+    petGate.write(gateEnd);
+    delay(gateOpenTime);
+    petGate.write(gateStart);
+    delay(gateOpenTime);
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  petGate.attach(2);
+  petGate.write(gateStart);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
